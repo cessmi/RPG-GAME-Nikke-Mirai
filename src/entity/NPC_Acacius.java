267eab754db8891bd84d94682ff1b4d31fd7672b
kyla <c202301063@iacademy.edu.ph>
2025-03-13 21:12:07 +0800

    changed acacius dialouge

--- src/entity/NPC_Acacius.java
+++ src/entity/NPC_Acacius.java
@@ -25,11 +25,15 @@ public class NPC_Acacius extends Entity{
 
     public void setDialouge(){
 
-        dialouges[0] = "Oh...a new one";
-        dialouges[1] = "I miss my farm";
-        dialouges[2] = "I wonder how she's doing";
-        dialouges[3] = "I'm...innocent";
-        dialouges[4] = "...";
+        dialouges[0] = "You there...\nanother lost soul wandering the wreckage?\nHah. Not much of a talker, are you?";
+        dialouges[1] = "Good. Words do nothing here but echo.";
+        dialouges[2] = "Do you know what it's like\nto scream your innocence, only to have\nyour voice drowned by the roar of the\ncrowd? ";
+        dialouges[3] = "To stand at the edge of the noose,\nfeeling the rope tighten before\nthe world goes black?";
+        dialouges[4] = "I was a fool.\nI thought justice was something real.\n";
+        dialouges[5] = "Something sharp enough to cut through lies.\nBut the truth is a brittle thing.\nIt breaks under the weight of men’s\nfears and ambitions";
+        dialouges[6] = "And now?\nNow, I ride this train with the rest of the\ndamned, waiting for peace that will never\ncome";
+        dialouges[7]= "Tell me…\nif I was truly innocent,\nwhy does my soul still linger?";
+        dialouges[8]= "...";
     }
 
     @Override
